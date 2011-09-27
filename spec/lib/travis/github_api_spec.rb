8dd1c93fd52a1ea76e2339c89cdf823182410d69
Sergio Arbeo <serabe@gmail.com>
2011-09-28 01:56:29 +0200

    Add specs to organization_name= behaviour.
    I also refactored the specs since we were using almost the same.

--- spec/lib/travis/github_api_spec.rb
+++ spec/lib/travis/github_api_spec.rb
@@ -3,28 +3,43 @@ require 'spec_helper'
 # TODO does this really belong into the Travis namespace?
 describe Travis::GithubApi do
   describe "repositories_for_user" do
+
+    before(:each) do
+      @user_repo1 = stub("First User Repo")
+      @user_repo2 = stub("Second User Repo")
+
+      @organization1 = stub("Travis Organization", :login => 'travis', :name => 'TravisCI')
+      @organization2 = stub("Github Organization", :login => 'github', :name => 'Github')
+      # to_ary is needed because of the flatten
+      @organization_repo1 = stub("Travis Organization Repo", :to_ary => nil)
+      @organization_repo1.stubs(:organization_name=).with("TravisCI")
+      @organization_repo2 = stub("Github Organization Repo", :to_ary => nil)
+      @organization_repo2.stubs(:organization_name=).with("Github")
+
+      Octokit.stubs(:repositories).with('fulanito').returns([@user_repo1, @user_repo2])
+      Octokit.stubs(:organizations).with('fulanito').returns([@organization1, @organization2])
+      Octokit.stubs(:organization_repositories).with('travis').returns([@organization_repo1])
+      Octokit.stubs(:organization_repositories).with('github').returns([@organization_repo2])
+    end
+
     it "should get the user repositories" do
-      repo1 = stub()
-      repo2 = stub()
-      
-      Octokit.stubs(:repositories).with('fulanito').returns([repo1, repo2])
-      Octokit.stubs(:organizations).with('fulanito').returns([])
-      
-      Travis::GithubApi.repositories_for_user('fulanito').should include(repo1, repo2)
+      Travis::GithubApi.repositories_for_user('fulanito').should include(@user_repo1, @user_repo2)
+    end
+
+    it "should never add an organization name to an user repository" do
+      @user_repo1.expects(:organization_name).never
+      @user_repo1.expects(:organization_name).never
+      Travis::GithubApi.repositories_for_user('fulanito')
     end
     
     it "should get the user's organization repositories" do
-      organization1 = stub(:login => 'travis')
-      organization2 = stub(:login => 'github')
-      repo1 = stub(:to_ary => nil)
-      repo2 = stub(:to_ary => nil)
-      
-      Octokit.stubs(:repositories).with('fulanito').returns([])
-      Octokit.stubs(:organizations).with('fulanito').returns([organization1, organization2])
-      Octokit.stubs(:organization_repositories).with('travis').returns([repo1])
-      Octokit.stubs(:organization_repositories).with('github').returns([repo2])
-      
-      Travis::GithubApi.repositories_for_user('fulanito').should include(repo1, repo2)
+      Travis::GithubApi.repositories_for_user('fulanito').should include(@organization_repo1, @organization_repo2)
+    end
+
+    it "should add an organization name to the repos from organizations" do
+      @organization_repo1.expects(:organization_name=).with("TravisCI").once
+      @organization_repo2.expects(:organization_name=).with("Github").once
+      Travis::GithubApi.repositories_for_user('fulanito')
     end
   end
 end
