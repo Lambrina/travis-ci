66c763df9f1bf298aa8871816f7bcaf1bfbff30b
Sergio Arbeo <serabe@gmail.com>
2011-10-09 17:47:16 +0200

    Modify map+flat to use flat_map.
    Besides, it uses again login as a parameter since authorized
    requests are not an option for it.
    
    As antares_ told me in #travis:
    
    so, just flat_map that shit!

--- lib/travis/github_api.rb
+++ lib/travis/github_api.rb
@@ -30,11 +30,11 @@ module Travis
       end
 
       def repositories_for_user(login)
-        Octokit.repositories(login) + Octokit.organizations(login).map do |organization|
+        Octokit.repositories(login) + Octokit.organizations(login).flat_map do |organization|
           Octokit.organization_repositories(organization.login).each do |repo|
             repo.organization_name = organization.name
           end
-        end.flatten
+        end
       end
     end
   end
